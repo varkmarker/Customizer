4858aa0868a36aec3de393724b096f670e4dcccc
varkmarker <121176482+varkmarker@users.noreply.github.com>
2024-08-01 10:18:30 -0400

    Update update.py
    Add new statement choice

--- items/update/update.py
+++ items/update/update.py
@@ -1,35 +1,12 @@
 #! /usr/bin/python3
-from colr import Colr as colr
 import os
+from colr import Colr as colr
 
 
+# Color's functions
 class Colors:
     def red(data):
         print(colr().hex("#ff0000", data, rgb_mode=True))
-
-    def rose(data):
-        print(colr().hex("#ff0066", data, rgb_mode=True))
-
-    def green(data):
-        print(colr().hex("#00ff8d", data, rgb_mode=True))
-
-    def gnome_green(data):
-        print(colr().hex("#2ed1b4", data, rgb_mode=True))
-
-    def yellow_green(data):
-        print(colr().hex("#a8c836", data, rgb_mode=True))
-
-    def dark_orange(data):
-        print(colr().hex("#cf301b", data, rgb_mode=True))
-
-    def light_gnome(data):
-        print(colr().hex("#00ffc4", data, rgb_mode=True))
-
-    def yellow_green(data):
-        print(colr().hex("#7ed666", data, rgb_mode=True))
-
-    def violet(data):
-        print(colr().hex("#cc33ff", data, rgb_mode=True))
 
     def light_green(data):
         print(colr().hex("#21ff00", data, rgb_mode=True))
@@ -37,76 +14,145 @@ class Colors:
     def orange(data):
         print(colr().hex("#ff8e35", data, rgb_mode=True))
 
-    def yellow(data):
-        print(colr().hex("#fff300", data, rgb_mode=True))
-
-    def sky_blue(data):
-        print(colr().hex("#00ccff", data, rgb_mode=True))
-
-    def blue(data):
-        print(colr().hex("#0000ff", data, rgb_mode=True))
-
     def cream(data):
         print(colr().hex("#ff9999", data, rgb_mode=True))
-
-    def dark_rose(data):
-        print(colr().hex("#cc0066", data, rgb_mode=True))
-
-    def dark_red(data):
-        print(colr().hex("#cc0000", data, rgb_mode=True))
-
-    def dark_green(data):
-        print(colr().hex("#009933", data, rgb_mode=True))
 
     def light_blue(data):
         print(colr().hex("#6666ff", data, rgb_mode=True))
 
 
-# Privileges check
-if os.geteuid() != 0:
-    Colors.red(" \n THIS SCRIPT REQUIRES SUDO PRIVILEGES . \n")
-    exit(1)
-else:
-    Colors.orange("\n UPDATE AND UPGRADE = y")
-    Colors.orange("\n UPGRADE = n")
-    Colors.orange("\n EXIT = e")
-    choice = input(colr().hex("#ff8e35", "\n > ", rgb_mode=True))
+def exit_line():
+    print(
+        colr().hex("#ff0000", "\n\n YOU ARE EXITING FROM OUR PROGRAM  ", rgb_mode=True),
+        colr().hex("#ff8e35", "(^-^)\n\n", rgb_mode=True),
+    )
+
+
+def update():
+    Colors.orange("\n           UPDATE STARTED")
+    Colors.light_green("")
+    os.system("sudo apt-get update")
+    Colors.orange("\n           UPDATE FINISHED")
+
+
+def upgrade():
+    Colors.orange("\n           UPGRADE STARTED")
+    Colors.light_green("")
+    os.system(
+        "sudo apt-get full-upgrade && sudo apt-get autoremove -y && sudo apt-get clean -y && sudo apt-get remove -y"
+    )
+    Colors.red(
+        "\n\n IF YOU ABORT UPGRADING STEP \n YOU MUST UPGRADE SOMETIME LATER \n OTHERWISE YOUR SYSTEM WILL SLOW AND ALSO SOME LAG\n"
+    )
+
+
+def upgrade_q():
+    choice = input(
+        colr().hex("#ff8e35", "\n If you want upgrade y or n : ", rgb_mode=True)
+    )
     choice = choice.lower()
     if choice == "y":
-
-        def update():
-            Colors.blue("\n  [+] UPDATE STARTED")
-            Colors.red("")
-            os.system("sudo apt-get update")
-            Colors.blue("\n  [+] UPDATE FINISHED")
-
-        update()
-        choice = input(
-            colr().hex("#0000ff", "\n If you want upgrade y or n : ", rgb_mode=True)
-        )
-        choice = choice.lower()
-        if choice == "y":
-
-            def upgrade():
-                Colors.blue("\n    [+] UPGRADE STARTED")
-                Colors.red("")
-                os.system("sudo apt-get full-upgrade")
-                Colors.blue("\n    [+] UPGRADE FINISHED")
-
-            upgrade()
-        else:
-            Colors.red("\n YOUR SYSTEM IS NOT UPGRADE \n")
-
-    elif choice == "n":
-
-        def upgrade():
-            Colors.blue("\n  [+] UPGRADE STARTED")
-            Colors.red("")
-            os.system("sudo apt-get full-upgrade")
-            Colors.blue("\n  [+] UPGRADE FINISHED")
-
         upgrade()
-    elif choice == "e":
-        Colors.red(" \n Exited \n")
+    elif choice == "n":
+        Colors.red("\n YOUR SYSTEM IS NOT UPGRADE")
     else:
-        Colors.red(" \n You press the wrong key \n")
+        Colors.red(" \n YOU PRESS THE WRONG KEY ")
+        print(
+            colr().hex("#ff0000", " INVALID KEY :", rgb_mode=True),
+            colr().hex("#6666ff", choice, rgb_mode=True),
+        )
+        upgrade_q()
+
+
+def restart():
+    Colors.orange(
+        "\n Check Any software is open or note Your are go to the restart your computer "
+    )
+    choice = input(
+        colr().hex("#ff8e35", "\n If you want restart y or n : ", rgb_mode=True)
+    )
+    choice = choice.lower()
+    if choice == "y":
+        os.system("reboot")
+    elif choice == "n":
+        Colors.red(
+            "\n You system is not restart you need to restart after some time for better performance"
+        )
+        exit_line()
+    else:
+        Colors.red(" \n YOU PRESS THE WRONG KEY ")
+        print(
+            colr().hex("#ff0000", " INVALID KEY :", rgb_mode=True),
+            colr().hex("#6666ff", choice, rgb_mode=True),
+        )
+        restart()
+
+
+def upgrade_choice_reboot_q():
+    choice = input(
+        colr().hex("#ff8e35", "\n If you want upgrade y or n : ", rgb_mode=True)
+    )
+    choice = choice.lower()
+    if choice == "y":
+        upgrade()
+        restart()
+    elif choice == "n":
+        Colors.red("\n YOUR SYSTEM IS NOT UPGRADE")
+        exit_line()
+    else:
+        Colors.red(" \n YOU PRESS THE WRONG KEY ")
+        print(
+            colr().hex("#ff0000", " INVALID KEY :", rgb_mode=True),
+            colr().hex("#6666ff", choice, rgb_mode=True),
+        )
+        upgrade_choice_reboot_q()
+
+
+if os.geteuid() != 0:
+    exit(
+        colr().hex(
+            "#ff0000",
+            "\n You need to have root privileges to run this script.\n Please try again, this time using 'sudo'. Exiting.",
+            rgb_mode=True,
+        )
+    )
+# Main menu
+
+Colors.cream("\n	 CMD SYSTEM UPDATER")
+Colors.orange("\n UPDATE = y")
+Colors.orange(" UPGRADE = n")
+Colors.orange("\n UPDATE AND UPGRADE = yn")
+Colors.orange(" UPGRADE AND RESTART = nr")
+Colors.orange("\n UPDATE AND UPGRADE AND RESTART = ynr")
+Colors.red(" EXIT = e")
+try:
+    choice = input(colr().hex("#ff8e35", "\n UPDATE OR UPGRADE $  ", rgb_mode=True))
+    choice = choice.lower()
+    if choice == "y":
+        update()
+        exit_line()
+    elif choice == "n":
+        upgrade_q()
+        exit_line()
+    elif choice == "nr":
+        upgrade_choice_reboot_q()
+    elif choice == "yn":
+        update()
+
+        upgrade_q()
+        exit_line()
+    elif choice == "ynr":
+        update()
+        upgrade_choice_reboot_q()
+
+    elif choice == "e":
+        exit_line()
+        exit()
+    else:
+        Colors.red(" \n YOU PRESS THE WRONG KEY ")
+        print(
+            colr().hex("#ff0000", " INVALID KEY :", rgb_mode=True),
+            colr().hex("#6666ff", choice, rgb_mode=True),
+        )
+except KeyboardInterrupt:
+    Colors.red("\n\n .......Keyboard Interrupted.....\n\n")
